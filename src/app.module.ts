32cb6a53fc63052d7901eb296f5aa6123aa737c2
BUGINGO Janvier <66186428+BugingoJanvier@users.noreply.github.com>
2025-11-05 18:10:34 +0200

    feat: add Tasks module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
 
 
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [],
   providers: [],
 })
